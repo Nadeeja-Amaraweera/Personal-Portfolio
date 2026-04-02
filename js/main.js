0924a2c2f400d7ac248323b56511b9c5dc586bb6
nadeeja <nadeejaamaraweera@gmail.com>
2026-04-02 22:29:49 +0530

    new

--- js/main.js
+++ js/main.js
@@ -33,3 +33,81 @@ const observer3 = new IntersectionObserver((entries) => {
 }, { threshold: 0.4 });
 
 skills.forEach(skill => observer3.observe(skill));
+
+// Works section navigation
+const projectBtn = document.getElementById('work-project-btn');
+const assignmentBtn = document.getElementById('work-assignment-btn');
+const projectContainer = document.querySelector('.project-container');
+const worksContainer = document.querySelector('.assignments-container');
+
+projectBtn.addEventListener('click', () => {
+    projectBtn.classList.add('active');
+    assignmentBtn.classList.remove('active');
+    projectContainer.classList.remove('hidden');
+    worksContainer.classList.add('hidden');
+});
+
+assignmentBtn.addEventListener('click', () => {
+    projectContainer.classList.add('hidden');
+    worksContainer.classList.remove('hidden');
+    assignmentBtn.classList.add('active');
+    projectBtn.classList.remove('active');
+});
+
+// Typing animation for home section
+
+// typing
+const text = "Nadeeja Amaraweera"; // Teks yang akan dianimasikan
+let index = 0;
+let isDeleting = false;
+const speed = 100; // Kecepatan mengetik
+const delayBeforeDeleting = 3000; // Waktu jeda sebelum menghapus
+const delayBeforeTypingAgain = 500; // Waktu jeda sebelum mengetik ulang
+
+function typeText() {
+  const typingTextElement = document.getElementById("typing-text");
+
+  if (isDeleting) {
+    typingTextElement.innerHTML = text.substring(0, index);
+    index--;
+    if (index < 0) {
+      isDeleting = false;
+      setTimeout(typeText, delayBeforeTypingAgain);
+    } else {
+      setTimeout(typeText, speed / 2); // Lebih cepat saat menghapus
+    }
+  } else {
+    typingTextElement.innerHTML = text.substring(0, index);
+    index++;
+    if (index > text.length) {
+      isDeleting = true;
+      setTimeout(typeText, delayBeforeDeleting); // Jeda sebelum mulai menghapus
+    } else {
+      setTimeout(typeText, speed); // Kecepatan mengetik
+    }
+  }
+}
+
+window.onload = function () {
+  setTimeout(typeText, delayBeforeTypingAgain); // Mulai mengetik setelah jeda
+};
+
+
+// Burger menu toggle
+const burgerBtn = document.getElementById("burgerBtn");
+const burgerLinks = document.querySelector(".burger-links");
+const navLinks = document.querySelectorAll(".links-navigation a");
+
+burgerBtn.addEventListener("click", () => {
+    burgerBtn.classList.toggle("active");
+    burgerLinks.classList.toggle("active");
+    
+});
+
+// close sidebar when clicking links
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        burgerBtn.classList.remove("active");
+        burgerLinks.classList.remove("active");
+    });
+});
